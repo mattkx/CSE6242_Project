b540735c29b76717cc968f8e8f614e91f45cf5e3
mattkx <93497222+mattkx@users.noreply.github.com>
2021-10-31 20:27:06 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+from dbconn import *
+
+conn = dbconn(uri="neo4j+s://7417a683.databases.neo4j.io", user="neo4j", pwd="7RKgGJvTQDrha88nsBG_6Qg4m7roIh7UYXtD2eE9T-8")
+
+#conn.query("CREATE OR REPLACE DATABASE citibikedb")
+
+
