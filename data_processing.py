0d4501208bf803791595014b9dbb32e44ffa6322
mattkx <93497222+mattkx@users.noreply.github.com>
2021-12-01 13:08:00 -0500

    Queries working

--- data_processing.py
+++ data_processing.py
@@ -2,7 +2,8 @@ import pandas as pd
 import pickle
 #from sklearn.ensemble import AdaBoostRegressor
 from py2neo import Graph
-
+import itertools
+import re
 #conn = dbconn(uri="neo4j+s://7417a683.databases.neo4j.io", user="neo4j", pwd="7RKgGJvTQDrha88nsBG_6Qg4m7roIh7UYXtD2eE9T-8")
 citibike = Graph("neo4j+s://7417a683.databases.neo4j.io", auth=("neo4j", "7RKgGJvTQDrha88nsBG_6Qg4m7roIh7UYXtD2eE9T-8"))
 def getDemand(args):
@@ -23,19 +24,36 @@ def getDemand(args):
     demand10 = sorted(demand, key=demand.get, reverse=True)[:10]
     
     return demand10
+def f(l, n):
+    return  [l[i:i+n] for i in range(len(l) - n + 1)] 
+def split_ids(df):
+    return re.findall("\((.*?)\)", df)
 
 def getNeo4JRoute(demand):
-
+    #demand = demand10
+   
+    trips = f(demand,2)
+    listoftrips = []
+    for stations in trips:
+        start = str(stations[0])
+        stop = str(stations[1])
     
-    query_string = '''
-    MATCH (s1:station { name: 'Brooklyn Bridge Park - Pier 2' })
-MATCH (s2:station { name: 'Rivington St & Ridge St' })
-CALL apoc.algo.dijkstra(s1, s2, 'NEAR', 'distance') YIELD path, weight
-RETURN path, weight
-
-'''
-    results = citibike.run(query_string)
-
+        query_string = '''
+        MATCH (s1:station) where id(s1) = '''+str(start)+'''
+    MATCH (s2:station) where id(s2) = '''+str(stop)+'''
+    CALL apoc.algo.dijkstra(s1, s2, 'NEAR', 'distance') YIELD path, weight
+    RETURN path, weight
     
+    '''
     
-    return routelist
+        results = citibike.run(query_string).to_data_frame()
+        listoftrips.append(results)
+    
+    alltrips = pd.concat(listoftrips)
+    alltrips['path'] = alltrips['path'].astype(str)
+    alltrips['ids'] = alltrips['path'].apply(split_ids)
+    routelist = alltrips.ids.sum()
+    routelist_final = list(dict.fromkeys(routelist))    
+    alltrips.to_csv('trips.csv', index = False)
+    return routelist_final
+
